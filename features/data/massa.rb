296a544caf1bcdba42df85934311a5248d922865
Thialison Souza <mac@Thialison-MacPro.local>
2017-10-12 10:32:35 -0300

    ajuste de estrutura

--- features/data/massa.rb
+++ features/data/massa.rb
@@ -1,6 +1,6 @@
 #encoding: utf-8
 
-MASSA ={
+MASSA = {
     veiculo_valida: {
         letra: "JQF",
         numero: "8956"
--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -18,12 +18,5 @@ Before do |scenario|
 end
 
 After do |scenario|
-  	time = Time.new
-	Dir.mkdir('report') unless Dir.exist?('report')
-
-	sufix = ('fail' if scenario.failed?) || 'sucess'
-	name = scenario.name.tr(' ','_').downcase
-
-	screenshot_embed(:name => "./report/#{sufix}_#{name}_#{time.day}-#{time.month}-#{time.year}")
     shutdown_test_server
 end
